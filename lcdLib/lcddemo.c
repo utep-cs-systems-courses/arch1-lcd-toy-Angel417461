b8dcd4247029563178291537d346952b594abb4a
Angel Rodriguez II <rodriguez4174@gmail.com>
2021-04-29 16:58:06 -0600

    11x16 font is functioning

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -16,8 +16,10 @@ main()
 
   clearScreen(COLOR_BLUE);
 
-  drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+  drawString5x7(80,20, "yo bro", COLOR_GREEN, COLOR_RED);
 
-  fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+  drawString11x16(5,20, "yo bro", COLOR_GREEN, COLOR_RED);
+
+  // fillRectangle(30,30, 60, 60, COLOR_ORANGE);
   
 }
