0c5e015b63daa44c816be36c49e6518256dd18d7
Angel Rodriguez II <rodriguez4174@gmail.com>
2021-05-04 17:11:17 -0600

    Makefile errors witht edt_handler

--- lcdLib/lcddemo2.c
+++ lcdLib/lcddemo2.c
@@ -6,11 +6,14 @@
 #include "lcdutils.h"
 #include "lcddraw.h"
 
+short redrawScreen = 1;
+u_int fontFgColor = COLOR_GREEN;
+
 void draw_christmas_tree(char offset_r, char offset_c){
   for(int r = 0; r< 40; r++){
     for (int c = 0; c <= r/2; c++){
-      drawPixel(offset_c + c, offset_r + r, COLOR_GREEN);
-      drawPixel(offset_c - c, offset_r + r, COLOR_GREEN);
+      drawPixel(offset_c + c, offset_r + r, COLOR_GREEN); //makes right half of the triangle
+      drawPixel(offset_c - c, offset_r + r, COLOR_GREEN); //makes left half side of the triangle
     }
   }
   for(int r = 0; r< 50; r++){
@@ -29,34 +32,59 @@ void draw_christmas_tree(char offset_r, char offset_c){
   fillRectangle(0, 135, 400, 40, COLOR_WHITE);
   
 }
+
+void wdt_c_handler()
+{
+  static int secCount = 0;
+
+  secCount ++;
+  if (secCount == 250) {		/* once/sec */
+    secCount = 0;
+    fontFgColor = (fontFgColor == COLOR_GREEN) ? COLOR_BLACK : COLOR_GREEN;
+    redrawScreen = 1;
+  }
+}
+
+
       
 /** Initializes everything, clears the screen, draws "hello" and a square */
 int main()
 {
+ 
+  
+  //P1DIR |= LED_GREEN;		/**< Green led on when CPU on */		
+  //P1OUT |= LED_GREEN;
+  // configureClocks();
+  //lcd_init();
+  
+  // enableWDTInterrupts();      /**< enable periodic interrupt */
+  //or_sr(0x8);	              /**< GIE (enable interrupts) */
+  
+  //clearScreen(COLOR_BLUE);
+  //while (1) {			/* forever */
+  //if (redrawScreen) {
+  //  redrawScreen = 0;
+  //  drawString5x7(20,20, "hello", fontFgColor, COLOR_BLUE);
+      //drawString5x7(15,20, "merry", COLOR_GREEN, COLOR_RED);
+  //  }
+    //P1OUT &= ~LED_GREEN;	/* green off */
+    //or_sr(0x10);		/**< CPU OFF */
+    //P1OUT |= LED_GREEN;		/* green on */
+    //}
+
+  
+
+
+  
   configureClocks();
   lcd_init();
   u_char width = screenWidth, height = screenHeight;
 
   clearScreen(COLOR_BLUE);
-
-  drawString5x7(15,20, "merry", COLOR_GREEN, COLOR_RED);
-  drawString5x7(3,29, "christmas", COLOR_GREEN, COLOR_RED);
-
-  //fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+  drawString11x16(1,10, "merry", COLOR_RED, COLOR_BLUE);
+  drawString5x7(3,29, "christmas", COLOR_RED, COLOR_BLUE);
+  drawString11x16(60,7, "~", COLOR_YELLOW, COLOR_BLUE);
   draw_christmas_tree(20,65);
   
-  u_char offset_r = 50, offset_c = 50;
-  for (int r = 0; r<=30; r++){
-    for(int c = 0; c<=(15-r); c++){
-      // drawPixel(offset_c + c, offset_r - r, COLOR_ORANGE);
-      //drawPixel(offset_c - c, offset_r - r, COLOR_ORANGE);
-      //drawPixel(c + offset_c, offset_r + r, COLOR_ORANGE);
-      //drawPixel(offset_c - c, offset_r + r, COLOR_ORANGE);
-      
-    }
-  }
-
-
-
   
 }
